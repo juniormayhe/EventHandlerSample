6bec2eaded414113217f279300a879ca8d5bc63c
Wanderley Mayhe Junior <juniormayhe@gmail.com>
2017-06-28 16:58:06 -0300

    initial commit

--- EventHandlerSample/Program.cs
+++ EventHandlerSample/Program.cs
@@ -1,15 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventHandlerSample
 {
     class Program
     {
+        //This is the type to be used by EventHandler.
+        //This event args will hold information about the event
+        class ThresholdEventArgs : EventArgs {
+            public DateTime WhenHappened { get; set; }
+            public int ValueGreaterThanThreshold { get; set; }
+        }
+
+        //a sample class where we'll get an event information
+        class Counter
+        {
+            private int total;
+            private const int THRESHOLD = 10;
+
+            //add a number to total, if number is greater than threshold, runs an event.
+            public void Add(int i)
+            {
+                total += i;
+
+                ThresholdEventArgs args = new ThresholdEventArgs();
+                args.WhenHappened = DateTime.Now;
+                args.ValueGreaterThanThreshold = total;
+
+                if (total > THRESHOLD)
+                {
+                    //runs event handler statements 
+                    this.OnThresholdReachedEventHandler?.Invoke(this, args);//suggested syntax
+
+                }
+                else {
+                    //runs event handler statements 
+                    EventHandler<ThresholdEventArgs> notReachedHandler = this.OnThresholdNotReachedEventHandler;//old syntax
+                    if (notReachedHandler != null)
+                        notReachedHandler(this, args);
+                }
+            }
+            
+            //event handlers must be public
+            public event EventHandler<ThresholdEventArgs> OnThresholdReachedEventHandler;
+            public event EventHandler<ThresholdEventArgs> OnThresholdNotReachedEventHandler;
+        }
+
         static void Main(string[] args)
         {
+            //test event handler
+            Counter counter = new Counter();
+
+            //attach your own methods to the counter's event handler for printing warning when threshold is reached
+            counter.OnThresholdReachedEventHandler += counter_OnThresholdReachedEventHandler;
+            counter.OnThresholdNotReachedEventHandler += counter_OnThresholdNotReachedEventHandler;
+
+            for (int i = 0; i < 10; i++)
+                counter.Add(i);
+
+            Console.Read();
         }
+
+        //when threshold is reached by Add method, prints a warning message
+        private static void counter_OnThresholdReachedEventHandler(object sender, ThresholdEventArgs e)
+        {
+            //here we have event handler statements
+            Console.WriteLine($"WARNING: Threshold reached at {e.WhenHappened}: Total is {e.ValueGreaterThanThreshold}.");
+        }
+
+        //when threshold is not reached, prints a message just for info
+        private static void counter_OnThresholdNotReachedEventHandler(object sender, ThresholdEventArgs e)
+        {
+            //here we have event handler statements
+            Console.WriteLine($"INFO: Threshold not reached at {e.WhenHappened}: Total is {e.ValueGreaterThanThreshold}.");
+        }
+
+        
     }
 }
--- EventHandlerSample/Properties/AssemblyInfo.cs
+++ EventHandlerSample/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EventHandlerSample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sample of Event Handlers in C# for printing messages if threshold is reached")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EventHandlerSample")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Wanderley Mayhe Junior <juniormayhe@gmail.com>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
